490bca84b1910ba3083f8ec13b14aea0f023b70d
Tomasz Sienkiewicz <tomasz.sienkiewicz73@gmail.com>
2023-12-14 22:16:46 +0100

    Dodaj wstępną strukturę folderów

---
+++ src/tsPwOS/tsPwOS.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "include/tsPwOS.h"
+
+int main(int /*argc*/, char* /*argv*/[]) {
+    std::cout << "tsPwOS - Przetrwaj w otwartym świecie..." << std::endl;
+    return EXIT_SUCCESS;
+}
